1bbc6dd7557d2914b4d31051b70a9f6a0efb1f07
David Ryder <david.winfield2000@gmail.com>
2022-03-15 02:23:04 -0400

    Added player movement script

---
+++ Game files/Rogue Like/Assets/playerMovement.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public int force;
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move up
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.AddForce(transform.up * force);
+        }
+        else if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            rb.AddForce(transform.up * -force);
+        }
+
+        // Move right
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            rb.AddForce(transform.right * force);
+        }
+        else if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            rb.AddForce(transform.right * -force);
+        }
+
+        // Move down
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            rb.AddForce(transform.up * -force);
+        }
+        else if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            rb.AddForce(transform.up * force);
+        }
+
+        // Move left
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            rb.AddForce(transform.right * -force);
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            rb.AddForce(transform.right * force);
+        }
+    }
+}
