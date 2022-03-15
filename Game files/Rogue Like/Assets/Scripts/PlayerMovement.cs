10aaa84f77afc7b0a40d758fd9979f8df6a7f839
David Ryder <david.winfield2000@gmail.com>
2022-03-15 11:36:30 -0400

    Small changes

--- Game files/Rogue Like/Assets/Scripts/PlayerMovement.cs
+++ Game files/Rogue Like/Assets/Scripts/PlayerMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnhancedPlayerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Transform player;
 
